1f6177a94e53446eda5bea665322ba7842cbaf69
xray <xyaguilar77@gmail.com>
2020-09-12 23:31:45 -0400

    created connection to mysql

---
+++ config/connection.js
@@ -0,0 +1,23 @@
+const mysql = require("mysql");
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection ({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "diegojoy777",
+        database: "burgers_db"
+    
+    })
+}
+
+connection.connect(function(err) {
+    if (err) {
+        console.error("connected as id" + err.stack);
+        return;
+    }
+    console.log("connected as id" + connection.threadId);
+});
+
+module.exports = connection;
---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheesburger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Chili Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Luther Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bison Burger', FALSE);
