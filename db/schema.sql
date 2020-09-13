52ddacf6831faa57ab27c2f1005a87925f643dd5
xray <xyaguilar77@gmail.com>
2020-09-12 23:59:47 -0400

    drop database

--- db/schema.sql
+++ db/schema.sql
@@ -1,12 +1,11 @@
+DROP DATABASE IF EXISTS burgers_db;
 CREATE DATABASE burgers_db;
 USE burgers_db;
 
-CREATE TABLE burgers
-(
-  id int NOT NULL AUTO_INCREMENT,
-  burger_name varchar(255) NOT NULL,
-  devoured BOOLEAN DEFAULT false,
+CREATE TABLE burgers (
+  id INT NOT NULL AUTO_INCREMENT,
+  burger_name VARCHAR(255) NOT NULL,
+  devoured BOOLEAN NOT NULL,
   PRIMARY KEY(id) 
 )
 
-SELECT * FROM burgers_db;
